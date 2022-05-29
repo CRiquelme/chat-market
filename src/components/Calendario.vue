c2d1b87d477cbc3161bcb77a514f52a202aa07ce
Carlos Riquelme <criquelme2@gmail.com>
2022-05-29 01:08:56 -0300

    Calendario en español de lunes a viernes

--- src/components/Calendario.vue
+++ src/components/Calendario.vue
@@ -26,22 +26,22 @@
             </template>
             <v-list>
               <v-list-item @click="type = 'day'">
-                <v-list-item-title>Day</v-list-item-title>
+                <v-list-item-title>Día</v-list-item-title>
               </v-list-item>
               <v-list-item @click="type = 'week'">
-                <v-list-item-title>Week</v-list-item-title>
+                <v-list-item-title>Semana</v-list-item-title>
               </v-list-item>
               <v-list-item @click="type = 'month'">
-                <v-list-item-title>Month</v-list-item-title>
+                <v-list-item-title>Mes</v-list-item-title>
               </v-list-item>
               <v-list-item @click="type = '4day'">
-                <v-list-item-title>4 days</v-list-item-title>
+                <v-list-item-title>4 días</v-list-item-title>
               </v-list-item>
             </v-list>
           </v-menu>
         </v-toolbar>
       </v-sheet>
-      <v-sheet height="600">
+      <v-sheet height="560">
         <v-calendar
           ref="calendar"
           v-model="focus"
@@ -55,6 +55,8 @@
           @click:more="viewDay"
           @click:date="viewDay"
           @change="updateRange"
+          :weekdays="weekday"
+          :interval-minutes= 45
         ></v-calendar>
 
         <!-- Agregar Modal Agregar Evento -->
@@ -117,10 +119,10 @@
       focus: new Date().toISOString().substr(0, 10),
       type: 'month',
       typeToLabel: {
-        month: 'Month',
-        week: 'Week',
-        day: 'Day',
-        '4day': '4 Days',
+        month: 'Mes',
+        week: 'Semana',
+        day: 'Día',
+        '4day': '4 Días',
       },
       start: null,
       end: null,
@@ -133,7 +135,8 @@
       details: null,
       color: '#1976D2',
       dialog: false,
-      currentlyEditing: null
+      currentlyEditing: null,
+      weekday: [1, 2, 3, 4, 5],
     }),
     computed: {
       title () {
